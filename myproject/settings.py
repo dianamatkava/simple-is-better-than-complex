6a5725d238a78785d85b109c0767d0c51deb3385
Diana Matkava <88348384+dianamatkava@users.noreply.github.com>
2022-09-26 23:22:06 +0200

    set up the postgres database and migrated the data

--- myproject/settings.py
+++ myproject/settings.py
@@ -146,8 +146,13 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', #postgresql_psycopg2
+        # 'NAME': BASE_DIR / 'db.sqlite3'
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
